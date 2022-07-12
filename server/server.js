1fdd272bfac8ef5201a49db105c4d181b2d1f8a3
Bernardo Glz de Castilla <67803645+BernardoGdc@users.noreply.github.com>
2022-07-12 11:36:44 -0500

    get selected playlists

--- server/server.js
+++ server/server.js
@@ -9,8 +9,48 @@ app.use(cors())
 app.use(bodyParser.json())
 app.listen(3001)
 
+app.post('/playlists', (req, res) => {
+    const accessToken = req.body.accessToken
+    const user = req.body.user
+    console.log("ACCESS TOKEN: ", accessToken)
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000/",
+        clientId: "03f702dcb5104eabbde108532dec5f64",
+        clientSecret: "a9e369b4b0304790b54e0c6dece146e6",
+    })
+    spotifyApi.setAccessToken(accessToken)
+
+    spotifyApi.getUserPlaylists(user).then((data) => {
+        res.json({
+            items: data.body.items
+        })
+        //console.log('Some information about this user', data.body.items);
+    }).catch(err => {
+        console.log(err)
+        res.sendStatus(402)
+    })
+})
+
+app.post('/getuser', (req, res) => {
+    const accessToken = req.body.accessToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000/",
+        clientId: "03f702dcb5104eabbde108532dec5f64",
+        clientSecret: "a9e369b4b0304790b54e0c6dece146e6",
+    })
+    spotifyApi.setAccessToken(accessToken)
+
+    spotifyApi.getMe().then((data) => {
+        res.json({
+            id: data.body.id
+        })
+    }).catch(err => {
+        console.log(err)
+        res.sendStatus(402)
+    })
+})
+
 app.post('/refresh', (req, res) => {
-    console.log("hi")
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
         redirectUri: "http://localhost:3000/",
@@ -21,7 +61,7 @@ app.post('/refresh', (req, res) => {
 
     spotifyApi.refreshAccessToken().then((data) => {
         res.json({
-            accesstoken:data.body.access_token,
+            accessToken:data.body.access_token,
             expiresIn: data.body.expiresIn
         })
     }).catch(() => {
